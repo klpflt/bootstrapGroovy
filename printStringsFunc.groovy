9c39f24a7beefa7c8a9177477454465c666df193
Bit Drop <bitdrop@acceptancecriteria.com>
2018-02-11 16:31:25 -0500

    Function to print diff strings

---
+++ printStringsFunc.groovy
@@ -0,0 +1,8 @@
+// Define a function to print something
+def printSomething(thing) {
+  println thing
+}
+
+// Print some strings
+printSomething("I like turtles")
+printSomething("I like squirrels")
