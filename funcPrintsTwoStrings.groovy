fa56331a9233949253e94acfd66b0b9896d413b9
Bit Drop <bitdrop@acceptancecriteria.com>
2018-02-11 17:14:53 -0500

    concatenation example with strings

---
+++ funcPrintsTwoStrings.groovy
@@ -0,0 +1,8 @@
+// Define a function that prints two things
+def printTwoThings(thingOne, thingTwo) {
+  println thingOne
+  println thingTwo
+}
+
+// Print something with two strings
+printTwoThings("I like", "turtles")
---
+++ twoThingsWithConcatenation.groovy
@@ -0,0 +1,8 @@
+// Deinf a function to print two things with concatenation
+def printTwoThings(thingOne, thingTwo) {
+  def things = thingOne + " " + thingTwo
+  println things
+}
+
+// Print the two things with concatenation
+printTwoThings("I like", "turtles")
