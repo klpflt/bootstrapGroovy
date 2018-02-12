6ba55053d726e97256b8c505d8bc20de9f113d78
Bit Drop <bitdrop@acceptancecriteria.com>
2018-02-11 21:23:02 -0500

    using the length method

---
+++ lengthMethod.groovy
@@ -0,0 +1,12 @@
+// Get the length of a raw string
+
+println "I like turtles".length()
+println "Build to Spill and Modest Mouse are great".length()
+
+// Define a new string
+def myString = "Sench is delicious, but it's difficult to make right"
+println myString.length()
+
+// Creat a new variable, containin the length of myString
+def myLength = myString.length()
+println myLength
