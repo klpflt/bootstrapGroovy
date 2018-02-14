634f08da5a9e159faf001aa053dc5db4df0016cf
Bit Drop <bitdrop@acceptancecriteria.com>
2018-02-13 21:17:30 -0500

    add not concatenate numbers

---
+++ addSubtract.groovy
@@ -0,0 +1,9 @@
+// Concatenate two numbers
+def concatenateNumbers(numberOne, numberTwo) {
+  println numberOne + numberTwo
+}
+
+def firstNumber = 5 - 3
+def secondNumber = 10 + 10
+
+concatenateNumbers(firstNumber, secondNumber)
