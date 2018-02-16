4fecaa15c0c5ca617559fa1c0758e73a6cb3212c
Bit Drop <bitdrop@acceptancecriteria.com>
2018-02-16 12:29:18 -0500

    example of while loop

---
+++ whileLooping.groovy
@@ -0,0 +1,9 @@
+// Example of while loops. Still feels familiar enough
+def count = 0
+
+while(count < 5) {
+  count = count + 1
+  println "Added 1 to " + count + "!"
+}
+
+println "We counted to five!"
