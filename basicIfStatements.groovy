992822d3266484e3d26a6dc835eaa72783698085
Bit Drop <bitdrop@acceptancecriteria.com>
2018-02-16 10:38:28 -0500

    basic if statements

---
+++ basicIfStatements.groovy
@@ -0,0 +1,15 @@
+// Basic if statements
+def myString = "I like turtles"
+
+println "Print something if myString contains 'turtles'"
+
+if(myString.contains("turtles")) {
+  println "My string contains turtles!"
+}
+
+println "Print something if myString contains 'turtles'"
+
+def containsBiscuits = myString.contains("biscuits")
+if(containsBiscuits) {
+  println "My string contains biscuits"
+}
