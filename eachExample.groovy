755e2a5f784955a012259c59574d72c39532479b
Bit Drop <bitdrop@acceptancecriteria.com>
2018-02-16 13:17:31 -0500

    each with biscuits iterator

---
+++ eachExample.groovy
@@ -0,0 +1,8 @@
+// Using each, and checking out collection iterators
+
+def biscuits = [ "rich tea", "digestive", "hobknobs" ]
+
+println "We have the following kinds of biscuits: "
+biscuits.each { biscuit ->
+  println biscuit
+}
