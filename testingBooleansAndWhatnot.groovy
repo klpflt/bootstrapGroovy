37fd5e329da3040b757e2b8ba8c23606dd89e20a
Bit Drop <bitdrop@acceptancecriteria.com>
2018-02-16 12:01:16 -0500

    testing with booleaning y'all!

---
+++ testingBooleansAndWhatnot.groovy
@@ -0,0 +1,18 @@
+def myAge = 5
+def myName = "Bertrand Russell"
+
+if(myAge < 12) {
+  println "Don't post comments on youtube"
+} else if(myAge >= 75) {
+  println"Don't go to youtube, you'll be horrified"
+} else {
+  println "You probably shouldn't post comments on youtube anyway"
+}
+
+if(myName == "Bertrand Russell") {
+  println "Proof is a necessarily weaker notion than truth!"
+}
+
+if(myName != "Zizek") {
+  println "Enjoy Coca Cola!"
+}
